b1d0d3dc0ff0b490b0531af934a1b30db852e08e
Andre Smart <andre.smart@contexual.io>
2026-01-15 01:22:01 -0500

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('surveyorb-website');
+  protected readonly title = signal('Survey Orb');
 }
